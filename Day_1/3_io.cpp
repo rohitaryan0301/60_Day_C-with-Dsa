f37083e738b474eca8b69113cd41c9696ada8628
rohitaryan0301 <149464200+rohitaryan0301@users.noreply.github.com>
2024-02-19 01:04:58 +0530

    Add files via upload

---
+++ Day_1/3_io.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int mother;
+   cout<<"How much mother give "<<endl;
+   cin>>mother;
+   
+   int father;
+   cout<<"How much father give "<<endl;
+   cin>>father;
+   
+   int sum = father +mother;
+    cout<<"Total collection "<<sum<<endl;
+    return 0;
+}
