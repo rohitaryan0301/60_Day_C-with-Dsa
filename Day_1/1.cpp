63ee9078c0d62722ece1fcf51d79a338f517f608
rohitaryan0301 <149464200+rohitaryan0301@users.noreply.github.com>
2024-02-19 01:00:47 +0530

    Add files via upload

---
+++ Day_1/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int a; //declaration of data type
+   a=20; //initialization
+   cout<<"size of int "<<sizeof (a)<<endl;
+
+   float b;
+   b = 20.8;
+   cout<<"size of float "<<sizeof (b)<<endl;
+
+   char c;
+   cout<<"size of char "<<sizeof (c)<<endl;
+
+   bool d;
+   cout<<"size of bool "<<sizeof (d)<<endl;
+   
+}
