03aa0fd4c1449221202bce3cc1b3b02e5cf0bdab
rohitaryan0301 <149464200+rohitaryan0301@users.noreply.github.com>
2024-02-19 01:03:24 +0530

    Add files via upload

---
+++ Day_1/2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    short int ro;
+    long int ar;
+    cout<<"size of shortint "<<sizeof(ro)<<endl;
+    cout<<"size of longint "<<sizeof(ro)<<endl;
+    return 0;
+}
