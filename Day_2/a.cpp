896665db54f1d8319393dd3030b07b6b3cb955f3
rohitaryan0301 <149464200+rohitaryan0301@users.noreply.github.com>
2024-02-21 22:40:36 +0530

    Add files via upload

--- Day_2/a.cpp
+++ Day_2/a.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() {
 
+
+  
+   int saving;
+   cout<<"Enter your saving"<<endl;
+   cin>>saving;
+   if(saving>500) {
+      if(saving>1000){
+         cout<<"Non-Veg Party"<<endl;
+      }
+      else 
+ cout<<" Veg-Party"<<endl;
+   }
+   else 
+   cout<<"No Party"<<endl;
+   
+   return 0;
+}
